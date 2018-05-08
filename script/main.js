b901242188c7921a060cac22c449c53c5d8d27a1
Estela <estela.parrado@gmail.com>
2018-05-08 14:00:55 +0200

    send answer not working

--- script/main.js
+++ script/main.js
@@ -1,4 +1,3 @@
-
 /*API para los datos*/
 function getQuestions(callback) {
 
@@ -7,9 +6,9 @@ function getQuestions(callback) {
             id: 1,
             title: '¿Cuántos años tiene María?',
             answers: [
-                { id: 1, answer: '25' },
-                { id: 2, answer: '33' },
-                { id: 3, answer: '37' }
+                { id: 0, answer: '25' },
+                { id: 1, answer: '33' },
+                { id: 2, answer: '37' }
             ],
             correctAnswer: { id: 2 }
         },
@@ -17,9 +16,9 @@ function getQuestions(callback) {
             id: 2,
             title: '¿Cuál es la capital de Zambia?',
             answers: [
-                { id: 1, answer: 'Lusaka' },
-                { id: 2, answer: 'Harare' },
-                { id: 3, answer: 'Madrid' }
+                { id: 0, answer: 'Lusaka' },
+                { id: 1, answer: 'Harare' },
+                { id: 2, answer: 'Madrid' }
             ],
             correctAnswer: { id: 1 }
         },
@@ -27,9 +26,9 @@ function getQuestions(callback) {
             id: 3,
             title: '¿Cuál es el nombre completo de Freud?',
             answers: [
-                { id: 1, answer: 'Adolf' },
-                { id: 2, answer: 'Sefarad' },
-                { id: 3, answer: 'Sigmund' }
+                { id: 0, answer: 'Adolf' },
+                { id: 1, answer: 'Sefarad' },
+                { id: 2, answer: 'Sigmund' }
             ],
             correctAnswer: { id: 3 }
         },
@@ -37,13 +36,13 @@ function getQuestions(callback) {
             id: 4,
             title: '¿Cuál es el animal más rápido del mundo?',
             answers: [
-                { id: 1, answer: 'Guepardo' },
-                { id: 2, answer: 'León' },
-                { id: 3, answer: 'Tortuga' }
+                { id: 0, answer: 'Guepardo' },
+                { id: 1, answer: 'León' },
+                { id: 2, answer: 'Tortuga' }
             ],
             correctAnswer: { id: 1 }
         }
-    ]
+    ];
     callback(serverData);
 }
 
@@ -62,12 +61,8 @@ function showTimeInterval() {
     clock.innerHTML = totalTime;
     if (totalTime === 0) {
         renderQuestion();
-        clearInterval(counter);
-
     }
 }
-
-
 
 /*Botón inicio*/
 var startButton = document.querySelector('.start--button');
@@ -83,7 +78,6 @@ function showAndStartQuestionContainer() {
 startButton.addEventListener('click', showAndStartQuestionContainer);
 
 
-
 /*Botón siguiente pregunta*/
 var nextQuestionButton = document.querySelector('.next--question');
 var questionTitle = document.querySelector('.question--title');
@@ -96,15 +90,43 @@ function renderQuestion() {
     showTimeInterval();
     if (i < questions.length) {
         questionTitle.innerHTML = (questions[i].title);
-        for (let x = 0; x < questions[i].answers.length; x++) {
+        for (var x = 0; x < questions[i].answers.length; x++) {
             questionAnswers[x].innerHTML = (questions[i].answers[x].answer);
         }
         i++;
     }
 }
 
+
 nextQuestionButton.addEventListener('click', renderQuestion);
 
+/*Id de la respuesta del usuario*/
 
+var radioAnswersList = document.querySelectorAll('.input-radio');
+function listenerEvents() {
+    for (var i = 0; i < radioAnswersList.length; i++) {
+        radioAnswersList[i].addEventListener('click', saveAnswerData);
+    }
+}
 
+var userAnswerId;
+var answersOptions = document.querySelectorAll('.asnwer--option');
 
+function saveAnswerData(event) {
+    var answersListId = event.currentTarget.getAttribute('data-id');
+    userAnswerId = answersListId;
+}
+
+listenerEvents();
+
+/*Evento botón enviar*/
+var sendAnswerButton = document.querySelector('.send--answer');
+
+function isCorrect(question, userAnswerId) {
+    if (questions[i].correctAnswer.id === userAnswerId) {
+        console.log('Correcto!!');
+    } else {
+        console.log('Incorrecta :(');
+    }
+}
+sendAnswerButton.addEventListener('click', isCorrect);
