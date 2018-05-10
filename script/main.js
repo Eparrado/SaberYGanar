11c4bf9b2310b2c3b233a76cd8f33f46c2e0bb50
Carlos Ble <carlos@codesai.com>
2018-05-10 16:58:42 +0100

    end of massive refactor

--- script/main.js
+++ script/main.js
@@ -8,19 +8,82 @@ function application() {
     var questionAnswers;
     var radioAnswersList;
     var questionsIndex = 0;
+    var timerId;
+    var countdown;
+
 
     function start(){
         startButton = document.querySelector('.start--button');
+        startButton.addEventListener('click', onStartGame);
         questionsContainer = document.querySelector('.questions__container');
-        startButton.addEventListener('click', renderQuestion);
-        nextQuestionButton = document.getElementById('next--question--button');
         questionTitle = document.querySelector('.question--title');
         questionAnswers = document.querySelectorAll('.question--answer');
         radioAnswersList = document.querySelectorAll('.input-radio');
-        nextQuestionButton.addEventListener('click', renderQuestion);
+        nextQuestionButton = document.getElementById('next--question--button');
+        nextQuestionButton.addEventListener('click', onNextQuestion);
         getQuestions(function (data) {
             questions = data;
         });
+    }
+
+    function onStartGame(){
+        resetCountdown();
+        startTimer();
+        loadNextQuestion();
+    }
+    function onNextQuestion(){
+        loadNextQuestion();
+    }
+    function loadNextQuestion() {
+        goToNextQuestion();
+        resetCountdown();
+        if (isNotTheEndOfTheGame()) {
+            renderQuestion(currentQuestion());
+        }
+        else {
+            gameOver();
+        }
+    }
+    function gameOver(){
+        hideContainerPanel();
+        stopTimer();
+        resetQuestions();
+    }
+    function isNotTheEndOfTheGame(){
+        return questionsIndex < questions.length;
+    }
+    function resetQuestions(){
+        questionsIndex = 0;
+    }
+    function goToNextQuestion(){
+        questionsIndex++;
+    }
+    function currentQuestion() {
+        return questions[questionsIndex];
+    }
+    function startTimer() {
+        timerId = setInterval(function(){
+            updateCountdown(onNextQuestion, timeChanged);
+        }, 1000);
+    }
+    function stopTimer(){
+        clearInterval(timerId);
+    }
+    function resetCountdown(){
+        countdown = 10;
+    }
+    function timeChanged() {
+        var clock = document.querySelector('.clock');
+        clock.innerHTML = countdown;
+    }
+    function updateCountdown(onTimeout, onTimeChanged){
+        countdown--;
+        if (countdown > 0) {
+            onTimeChanged();
+        }
+        else if (countdown === 0) {
+            onTimeout();
+        }
     }
 
     function getQuestions(callback) {
@@ -70,35 +133,20 @@ function application() {
         callback(serverData);
     }
 
-    function renderQuestion() {
-        startCountdown();
-        questionsContainer.classList.remove('hidden');
-        if (questionsIndex < questions.length) {
-            questionTitle.innerHTML = (questions[questionsIndex].title);
-            questionTitle.setAttribute('id', questions[questionsIndex].id);
-            for (var x = 0; x < questions[questionsIndex].answers.length; x++) {
-                questionAnswers[x].innerHTML = (questions[questionsIndex].answers[x].answer);
-                radioAnswersList[x].setAttribute('id', questions[questionsIndex].answers[x].id);
-            }
-        } else {
-            questionsContainer.classList.toggle('hidden');
+    function renderQuestion(question) {
+        showContainerPanel();
+        questionTitle.innerHTML = (question.title);
+        questionTitle.setAttribute('id', question.id);
+        for (var x = 0; x < question.answers.length; x++) {
+            questionAnswers[x].innerHTML = (question.answers[x].answer);
+            radioAnswersList[x].setAttribute('id', question.answers[x].id);
         }
     }
-
-    function startCountdown() {
-        var initialTime = 10;
-        setInterval(function () {
-            initialTime--;
-            if (initialTime >= 0) {
-                var clock = document.querySelector('.clock');
-                clock.innerHTML = initialTime;
-            }
-            if (initialTime === 0) {
-                clearInterval(initialTime);
-                questionsIndex++;
-                renderQuestion();
-            }
-        }, 1000);
+    function showContainerPanel(){
+        questionsContainer.classList.remove('hidden');
+    }
+    function hideContainerPanel() {
+        questionsContainer.classList.toggle('hidden');
     }
 
     return {
